d6eb409b54b39c7b5d65f45d66761a0618999290
Mademti <Emmanuelzongmo55@gmail.com>
2025-05-23 23:30:55 -0400

    adding the map

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  late GoogleMapController mapController;
+
+  final LatLng _initialPosition = const LatLng(
+    37.7749,
+    -122.4194,
+  ); // San Francisco
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('blablabla'),
+        backgroundColor: Colors.green,
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _initialPosition,
+          zoom: 10,
+        ),
+      ),
     );
   }
 }
