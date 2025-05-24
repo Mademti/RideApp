38e57df8a66a978c4d5e0bf4ba51c952b3b7c120
Mademti <Emmanuelzongmo55@gmail.com>
2025-05-23 23:40:47 -0400

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -2,40 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
-  runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: MainApp()));
+  runApp(const MainApp());
 }
 
-class MainApp extends StatefulWidget {
+class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  State createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
-  late GoogleMapController mapController;
-
-  final LatLng _initialPosition = const LatLng(
-    37.7749,
-    -122.4194,
-  ); // San Francisco
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('blablabla'),
-        backgroundColor: Colors.green,
-      ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _initialPosition,
-          zoom: 10,
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: GoogleMap(
+          initialCameraPosition: CameraPosition(
+            target: LatLng(37.7749, -122.4194), // San Francisco coordinates
+            zoom: 10,
+          ),
         ),
       ),
     );
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,73 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
 
-import 'package:uber_app/main.dart';
+// import 'package:uber_app/main.dart';
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MainApp());
+// void main() {
+//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+//     // Build our app and trigger a frame.
+//     await tester.pumpWidget(const MainApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+//     // Verify that our counter starts at 0.
+//     expect(find.text('0'), findsOneWidget);
+//     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+//     // Tap the '+' icon and trigger a frame.
+//     await tester.tap(find.byIcon(Icons.add));
+//     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
+//     // Verify that our counter has incremented.
+//     expect(find.text('0'), findsNothing);
+//     expect(find.text('1'), findsOneWidget);
+//   });
+// }
+
+//
+//
+//
+//
+//
+//
+// void main() {
+//   runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: MainApp()));
+// }
+
+// class MainApp extends StatefulWidget {
+//   const MainApp({super.key});
+
+//   @override
+//   State createState() => _MainAppState();
+// }
+
+// class _MainAppState extends State<MainApp> {
+//   late GoogleMapController mapController;
+
+//   final LatLng _initialPosition = const LatLng(
+//     37.7749,
+//     -122.4194,
+//   ); // San Francisco
+
+//   void _onMapCreated(GoogleMapController controller) {
+//     mapController = controller;
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('blablabla'),
+//         backgroundColor: Colors.green,
+//       ),
+//       body: GoogleMap(
+//         onMapCreated: _onMapCreated,
+//         initialCameraPosition: CameraPosition(
+//           target: _initialPosition,
+//           zoom: 10,
+//         ),
+//       ),
+//     );
+//   }
+// }
